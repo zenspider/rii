37369968661dd71a584cda4458efcefa4682e390
Ryan Davis <ryand-ruby@zenspider.com>
2017-06-10 15:49:21 -0700

    Still working on base functionality.
    Added ARGV processing. Give it directories and it'll process all .rb
    files in them.
    
    Still doens't bother with a model.
    
    Fixed up class and method names using accessors in
    MethodBasedSexpProcessor.
    
    Fixed up method arg lists using ruby2ruby.
    
    Now doesn't print a method if it doesn't have documentation. Not sure
    on that.

--- doco.rb
+++ doco.rb
@@ -2,41 +2,65 @@
 
 $: << File.expand_path("~/Work/p4/zss/src/sexp_processor/dev/lib/")
 $: << File.expand_path("~/Work/p4/zss/src/ruby_parser/dev/lib/")
+$: << File.expand_path("~/Work/p4/zss/src/ruby2ruby/dev/lib/")
 
 require "ruby_parser"
 require "sexp_processor"
+require "ruby2ruby"
 require "pp"
 
 class Doco < MethodBasedSexpProcessor
+  attr_accessor :r2r
+
+  def initialize
+    super
+    self.r2r = Ruby2Ruby.new
+  end
+
   def massage_comment lines, level=2
-    prefix = "  " * level
-    lines.lines.map { |line| line.sub(/^ *#+ */, "") }.join prefix
+    prefix = "  " * (level+1)
+    s = lines.lines.map { |line| line.sub(/^ *#+ */, "") }.join(prefix).strip
+    s unless s.empty?
   end
 
   def process_class exp
-    _, name, _superklass, *_rest = exp
-    comment = massage_comment exp.comments
-    comment = "\n" if comment.empty?
+    super do
+      name = self.klass_name
+      comment = massage_comment exp.comments
 
-    puts "class %s:\n%s" % [name, comment]
+      if comment then
+        puts "  class %s:\n    %s" % [name, comment]
+      else
+        puts "  class %s:" % [name]
+      end
+      puts
 
-    super
+      process_until_empty exp
+    end
   end
 
   def process_defn exp
-    _, msg, args, *_body = exp
-    comment = massage_comment exp.comments
+    super do
+      args, *_body = exp
+      msg = self.method_name[1..-1]
+      comment = massage_comment exp.comments
 
-    puts "  def %s %p:\n%s" % [msg, process(args), comment]
+      if comment then
+        puts "    def %s %p:\n      %s" % [msg, args, comment]
+        puts
+      end
 
-    super
+      process_until_empty exp
+    end
   end
 end
 
 rp = RubyParser.for_current_ruby
 doco = Doco.new
 
-Dir["lib/**/*.rb"].sort.each do |path|
-  doco.process rp.parse File.read path
-  puts
+ARGV.each do |dir|
+  Dir["#{dir}/**/*.rb"].sort.each do |path|
+    puts path
+    doco.process rp.parse File.read(path), path
+  end
 end
